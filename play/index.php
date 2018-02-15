fc4aa46c6b3f8ce890b8c2577d3f580c28fc30c2
samn122 <you@example.com>
2018-02-14 21:45:33 -0500

    ???

--- play/index.php
+++ play/index.php
@@ -5,7 +5,7 @@
         header('Content-Type: application/json');
         $data = '{"grid" : [" "," "," ","X"," "," "," "," "," "], "winner" : " "}';
 
-        json_encode(json_decode($data));
+        echo json_encode(json_decode($data));
     } else {
         $data = $_POST['jsonData'];
         $response['grid'] = $data;
