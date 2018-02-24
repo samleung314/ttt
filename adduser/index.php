74b4de687a15c1591fad73e41b686e955cdb8adf
samn334 <you@example.com>
2018-02-24 18:48:14 -0500

    php response

--- adduser/index.php
+++ adduser/index.php
@@ -1,9 +1,14 @@
 <?php
 
-$user = $_POST['jsonData'];
-echo json_encode($user);
+if(empty($_POST)) {
 
+	header('Content-Type: application/json');
+	$data = '{"username":"Sam","password":"123","email":"samson.leung@stonybrook.edu"}';
+	echo ($data);
 
-
+}else {
+	$user = $_POST['jsonData'];
+	echo json_encode($user);
+}
 
 ?>
--- verify/index.php
+++ verify/index.php
@@ -1,6 +1,14 @@
 <?php
 
-$user = $_POST['jsonData'];
-echo json_encode($user);
+if(empty($_POST)) {
+
+	header('Content-Type: application/json');
+	$data = '{"email":"samson.leung@stonybrook.edu","key":"abracadabra"}';
+	echo ($data);
+
+}else {
+	$user = $_POST['jsonData'];
+	echo json_encode($user);
+}
 
 ?>
