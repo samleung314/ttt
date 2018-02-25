362edde90b45111cf2a7cee71369240f4d13b562
samn334 <you@example.com>
2018-02-24 19:46:42 -0500

    php response

--- adduser/index.php
+++ adduser/index.php
@@ -1,6 +1,14 @@
 <?php
-	$user = $_POST;
-	echo header('Content-Type: application/json');
-	echo http_response_code(202);
+
+if(empty($_POST)) {
+
+	header('Content-Type: application/json');
+	$data = '{"username":"sam","password":"123,"email":"samson.leung@stonybrook.edu"}';
+	echo ($data);
+
+}else {
+	$user = $_POST['jsonData'];
 	echo json_encode($user);
+}
+
 ?>
