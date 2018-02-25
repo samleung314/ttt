e429cc10a5d6bc3cc6249e7512a462956940a60b
samn334 <you@example.com>
2018-02-24 19:53:20 -0500

    php response

--- adduser/index.php
+++ adduser/index.php
@@ -3,8 +3,8 @@
 if(empty($_POST)) {
 
 	header('Content-Type: application/json');
-	$data = '{username:sam,password:123,email:samson.leung@stonybrook.edu}';
-	echo json_encode($data);
+	$data = '{ "username" : "sam", "password" : "1234", "email" : "samson.leung@stonybrook.edu"}';
+	echo ($data);
 
 }else {
 	$user = $_POST['jsonData'];
